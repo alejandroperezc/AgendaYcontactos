f9baea1b49b76b866d155f570c2b9e87db79cf56
alex <aleprez23@gmail.com>
2023-02-06 18:40:47 +0100

    .

---
+++ src/Controller/Controlador.java
@@ -0,0 +1,12 @@
+package Controller;
+
+public class Controlador {
+    public void aCorrer(){
+        Vista vista = new Vista();
+        Modelo modelo = new Modelo();
+
+        while(true) {
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import Controller.Controlador;
+
+public class Main {
+    public static void main(String[] args) {
+        new Controlador().aCorrer();
+    }
+}
